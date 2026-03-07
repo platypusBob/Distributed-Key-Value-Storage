8d189c5ef428979f557480e6de4e2a066043d133
Matheus Reato <matheus.r.bellanda@gmail.com>
2026-03-07 00:13:40 -0300

    Added djb2 function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "utils.h"
 
 int main() 
 {
-    printf("Hello, World!");
+    char name1[] = "Matheus";
+    char name2[] = "Mateus";
+
+    unsigned long hash1 = hash(name1);
+    unsigned long hash2 = hash(name2);
+
+    printf("%lu\n", hash1);
+    printf("%lu\n", hash2);
 }
